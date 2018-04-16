e13aed0e2faabf91921fc350aafb121bbddfd46c
Chetan Baadkar <37316635+ITchetan@users.noreply.github.com>
2018-04-17 10:28:00 +1200

    upated A

--- flaskProject/flaskProject.py
+++ flaskProject/flaskProject.py
@@ -8,7 +8,18 @@ app= Flask(__name__)
 def home():
     """
     Render a Hello World response.
-
     :return: Flask response
     """
     return 'Hello World!'
+
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+# @app.route('/login',methods=['GET','POST'])
+# def login():
+#     error = None
+#     if request.form=='POST':
+#         if reuest.form[username]!='admin'
+#     return render_template('login.html',error =error)
